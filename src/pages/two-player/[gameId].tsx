a726157dd12229656cc5a1612f039cf7e14e9c7c
AaronW <aaronwang96@hotmail.com>
2024-07-10 07:24:22 -0700

    initial two-player mode

--- src/pages/two-player/[gameId].tsx
+++ src/pages/two-player/[gameId].tsx
@@ -1,14 +1,20 @@
 /* eslint-disable @next/next/no-img-element */
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import Selections from '../components/Selections';
-import { generateGameId } from '../utils/generateGameId';
+import Selections from '../../components/Selections';
+import { FaClipboardList } from "react-icons/fa";
 
 export default function Home() {
   const router = useRouter();
   const [selection, setSelection] = useState<'Rock' | 'Paper' | 'Scissors' | null>(null);
   const [win, setWin] = useState(0);
   const [loss, setLoss] = useState(2);
+  const [url, setUrl] = useState('');
+
+  useEffect(() => {
+    const currentUrl = window.location.href;
+    setUrl(currentUrl);
+  }, []);
 
   const handleSelection = (choice: 'Rock' | 'Paper' | 'Scissors') => {
     setSelection(choice);
@@ -16,15 +22,32 @@ export default function Home() {
     setWin((prevScore) => prevScore + 1);
   };
 
-  const handlePlayWithFriend = () => {
-    const gameId = generateGameId();
-    router.push(`/two-player/${gameId}`);
+  const handlePlayWithBot = () => {
+    router.push(`/`);
   };
 
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between py-12 px-8 bg-custom-radial`}
     >
+      <div className="flex flex-col md:flex-row justify-center items-start md:items-center">
+        <div>
+          <label className="block text-white font-bold md:text-right mb-1 md:mb-0 pr-4">
+            Invite your Friend
+          </label>
+        </div>
+        <div className="flex items-center py-2 bg-white rounded">
+          <input
+            className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+            type="text"
+            placeholder={url}
+            disabled
+          />
+          <button className="flex-shrink-0 border-transparent border-4 text-blue-500 hover:text-blue-400 text-sm py-1 px-2 rounded hover:cursor-pointer" type="button">
+            <FaClipboardList />
+          </button>
+        </div>
+      </div>
       <div className="flex justify-between w-full md:w-7/12 max-w-xl p-6 border border-headerOutline rounded-lg">
         <div className="flex flex-col justify-center">
           <p className="font-barlow font-bold text-2xl md:text-3xl">ROCK</p>
@@ -46,9 +69,9 @@ export default function Home() {
       <div className="flex w-full justify-center md:justify-between gap-2">
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded hover:cursor-pointer"
-          onClick={handlePlayWithFriend}
+          onClick={handlePlayWithBot}
         >
-          PLAY WITH A FRIEND
+          PLAY WITH BOT
         </button>
         <button className="bg-transparent text-white font-semibold py-2 px-4 border border-headerOutline hover:border-white rounded hover:cursor-pointer">
           RULES
