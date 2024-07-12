7914b1bcd22a71fd27fc1ee500c1160b9bf83be3
AaronW <aaronwang96@hotmail.com>
2024-07-12 12:22:13 -0700

    specify white text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,9 +10,9 @@ const Header: React.FC<HeaderProps> = ({ win, loss }) => {
   return (
     <div className="flex justify-between w-full md:w-7/12 max-w-xl p-6 border border-headerOutline rounded-lg mb-6">
       <div className="flex flex-col justify-center">
-        <p className="font-bold text-2xl md:text-3xl">ROCK</p>
-        <p className="font-bold text-2xl md:text-3xl -my-3">PAPER</p>
-        <p className="font-bold text-2xl md:text-3xl">SCISSORS</p>
+        <p className="font-bold text-2xl md:text-3xl text-white">ROCK</p>
+        <p className="font-bold text-2xl md:text-3xl -my-3 text-white">PAPER</p>
+        <p className="font-bold text-2xl md:text-3xl text-white">SCISSORS</p>
       </div>
       <div className="flex flex-row gap-2">
         <div className="flex flex-col p-3 w-16 md:w-24 h-16 md:h-20 border rounded-lg justify-center items-center bg-white">
